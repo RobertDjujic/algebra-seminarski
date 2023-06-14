d2705f36ec2d25672bdd9c9a3e45ed37db7da675
Robert Đujić <robert.djujic@gmail.com>
2023-06-15 00:10:33 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
 import "./styles/styles.scss";
-import { randomColor } from "./utils/random-color";
-import { randomName } from "./utils/random-name";
-import { StateType } from "./data/types";
-import Messages from "./components/messages";
-
-const state: StateType = {
-  messages: [
-    {
-      text: "This is a test message!",
-      member: [
-        {
-          color: "blue",
-          username: "bluemoon",
-        },
-      ],
-    },
-  ],
-  member: [
-    {
-      username: randomName(),
-      color: randomColor(),
-    },
-  ],
-};
 
 const App = () => {
-  return (
-    <div className="messages">
-      <Messages messages={state.messages} currentMember={state.member} />
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
--- src/utils/random-color.ts
+++
@@ -1,3 +0,0 @@
-export const randomColor = () => {
-  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
-};
